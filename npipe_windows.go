c596bef659517382af5b85cab4f2ce897ff57cea
jeppeter <jeppeter@gmail.com>
2021-11-12 13:27:36 +0800

    run ok with different user server/client named pipe

--- npipe_windows.go
+++ npipe_windows.go
@@ -48,6 +48,7 @@ import (
 	"os"
 	"syscall"
 	"time"
+	"unsafe"
 )
 
 const (
@@ -97,6 +98,45 @@ const (
 
 	error_io_incomplete syscall.Errno = 0x3e4
 )
+
+const SECURITY_DESCRIPTOR_REVISION = 1
+
+var (
+	advapi32                         = syscall.NewLazyDLL("advapi32.dll")
+	procInitializeSecurityDescriptor = advapi32.NewProc("InitializeSecurityDescriptor")
+	procSetSecurityDescriptorDacl    = advapi32.NewProc("SetSecurityDescriptorDacl")
+)
+
+func initSecurityAttributes() (*syscall.SecurityAttributes, error) {
+
+	// create security descriptor
+	sd := make([]byte, 4096)
+	if res, _, err := procInitializeSecurityDescriptor.Call(
+		uintptr(unsafe.Pointer(&sd[0])),
+		SECURITY_DESCRIPTOR_REVISION); int(res) == 0 {
+
+		return nil, os.NewSyscallError("InitializeSecurityDescriptor", err)
+	}
+
+	// configure security descriptor
+	present := 1
+	defaulted := 0
+	if res, _, err := procSetSecurityDescriptorDacl.Call(
+		uintptr(unsafe.Pointer(&sd[0])),
+		uintptr(present),
+		uintptr(unsafe.Pointer(nil)), // acl
+		uintptr(defaulted)); int(res) == 0 {
+
+		return nil, os.NewSyscallError("SetSecurityDescriptorDacl", err)
+	}
+
+	var sa syscall.SecurityAttributes
+	sa.Length = uint32(unsafe.Sizeof(sa))
+	sa.SecurityDescriptor = uintptr(unsafe.Pointer(&sd[0]))
+
+	return &sa, nil
+
+}
 
 // PipeError is an error related to a call to a pipe
 type PipeError struct {
@@ -500,11 +540,12 @@ func createPipe(address string, first bool) (syscall.Handle, error) {
 	if first {
 		mode |= file_flag_first_pipe_instance
 	}
+	sa, err := initSecurityAttributes()
 	return createNamedPipe(n,
 		mode,
 		pipe_type_byte,
 		pipe_unlimited_instances,
-		512, 512, 0, nil)
+		512, 512, 0, sa)
 }
 
 func badAddr(addr string) PipeError {
